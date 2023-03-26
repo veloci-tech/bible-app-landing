af2f5788881161933c5c340b7a96d62e4906c7ea
Vlad <vladi03@hotmail.com>
2023-03-26 17:30:45 -0400

    starting point

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bible-app-landing
+load a web server that will autoload on change.  run with admin rights.
+<pre>
+npm install live-server -g
+</pre>
---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+// Create an example popover
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    console.log("one");
+  })
